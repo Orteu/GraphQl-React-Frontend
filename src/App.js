580b194436c8131c7e3fe4dc8dc02efa44efad17
Carlos <orteu15@gmail.com>
2019-06-21 23:57:45 +0200

    Feature: add react router and first route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
 import { ApolloProvider } from 'react-apollo';
 import ApolloClient from 'apollo-boost';
 
 // Imported components
 import Header from './components/Header';
 import Customers from './components/Customers';
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 const client = new ApolloClient({
   uri: "http://localhost:5000/graphql",
@@ -14,15 +16,23 @@ const client = new ApolloClient({
   },
 });
 
-function App() {
-  return (
-    <ApolloProvider client={client}>
-      <Header />
-      <div className="container">
-        <Customers />
-      </div>
-    </ApolloProvider>
-  );
+class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <Router>
+          <Fragment>
+            <Header />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Customers} />
+              </Switch>
+            </div>
+          </Fragment>
+        </Router>
+      </ApolloProvider>
+    );
+  }
 }
 
 export default App;
