632d57b323c156f05920c2fd344c87f98bad8c4f
Carlos <orteu15@gmail.com>
2019-06-28 22:43:53 +0200

    NotFinished: getCustomerById for edit screen

--- src/components/EditCustomer/EditCustomer.js
+++ src/components/EditCustomer/EditCustomer.js
@@ -1,9 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { Query } from 'react-apollo';
+import { CUSTOMER_GET_BY_ID } from '../../queries';
 
 export default class EditCustomer extends Component {
-    render() {
-        return(
-            <p>Edit Customer</p>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+
+	render() {
+		const { id } = this.props.match.params;
+		return (
+			<Fragment>
+				<p>Edit Customer</p>
+
+				<Query query={CUSTOMER_GET_BY_ID} variables={{ id }}>
+					{({ loading, error, data }) => {
+            if (loading) {
+              return 'Loading...'
+            }
+            if (error) {
+              return `Error ${error}`
+            }
+            console.log(data);
+          }}
+				</Query>
+			</Fragment>
+		);
+	}
 }
--- src/queries/index.js
+++ src/queries/index.js
@@ -1,10 +1,22 @@
 import gql from 'graphql-tag';
 
 export const CUSTOMERS_QUERY = gql`{
-    getAllCustomers{
+  getAllCustomers{
+    id
+    name
+    surname
+    email
+  }
+}`;
+
+export const CUSTOMER_GET_BY_ID = gql`
+  query getCustomerById($id: ID){
+    getCustomerById(id: $id){
       id
       name
       surname
       email
+      type
     }
-  }`;
+  }
+`;
