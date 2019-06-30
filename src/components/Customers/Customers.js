fdc0531b1cee8c1f14347a2e81209ae657a4d7fb
Carlos <orteu15@gmail.com>
2019-06-30 14:35:20 +0200

    NotFinished: paginator structure

--- src/components/Customers/Customers.js
+++ src/components/Customers/Customers.js
@@ -1,60 +1,69 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 import { Query, Mutation } from 'react-apollo';
 import { Link } from 'react-router-dom';
 
 import { CUSTOMERS_QUERY } from '../../queries';
 import { REMOVE_CUSTOMER } from '../../mutations';
 
-const Customers = () => (
-	<Query query={CUSTOMERS_QUERY} pollInterval={100}>
-		{({ loading, error, data, startPolling, stopPolling }) => {
-			if (loading) {
-				return '...loading';
+export default class Customers extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			paginator: {
+				currentPage: 1,
+				offset: 0
 			}
-			if (error) {
-				return `Error: ${error.message}`;
-			}
-			return (
-				<Fragment>
-					<h2 className="text-center">Customers list</h2>
-					<ul className="list-group mt-4">
-						{data.getAllCustomers.map((customer) => (
-							<li key={customer.id} className="list-group-item">
-								<div className="row justify-content-between align-items-center">
-									<div className="col-md-8 d-flex justify-content-between align-items-center">
-										{`${customer.name} ${customer.surname}`}
-									</div>
-									<div className="col-md-4 d-flex justify-content-end">
-                                        <Mutation
-                                            mutation={REMOVE_CUSTOMER}
-                                        >
-                                            {
-                                                (removeCustomer) => (
-                                                    <button
-                                                        className="btn btn-danger d-block d-md-inline-block mr-2"
-                                                        onClick={() => removeCustomer({ variables: {id: customer.id }})}
-                                                        type="button"
-                                                    >
-                                                            &times; Remove
-                                                    </button>
-                                                )
-                                            }
-                                        </Mutation>
-										<Link
-											to={`/customer/edit/${customer.id}`}
-											className="btn btn-success d-block d-md-inline-block"
-										>
-											Edit customer
-										</Link>
-									</div>
-								</div>
-							</li>
-						))}
-					</ul>
-				</Fragment>
-			);
-		}}
-	</Query>
-);
+		};
+	}
 
-export default Customers;
+	render() {
+		return (
+			<Query query={CUSTOMERS_QUERY} pollInterval={100}>
+				{({ loading, error, data, startPolling, stopPolling }) => {
+					if (loading) {
+						return '...loading';
+					}
+					if (error) {
+						return `Error: ${error.message}`;
+					}
+					return (
+						<Fragment>
+							<h2 className="text-center">Customers list</h2>
+							<ul className="list-group mt-4">
+								{data.getAllCustomers.map((customer) => (
+									<li key={customer.id} className="list-group-item">
+										<div className="row justify-content-between align-items-center">
+											<div className="col-md-8 d-flex justify-content-between align-items-center">
+												{`${customer.name} ${customer.surname}`}
+											</div>
+											<div className="col-md-4 d-flex justify-content-end">
+												<Mutation mutation={REMOVE_CUSTOMER}>
+													{(removeCustomer) => (
+														<button
+															className="btn btn-danger d-block d-md-inline-block mr-2"
+															onClick={() =>
+																removeCustomer({ variables: { id: customer.id } })}
+															type="button"
+														>
+															&times; Remove
+														</button>
+													)}
+												</Mutation>
+												<Link
+													to={`/customer/edit/${customer.id}`}
+													className="btn btn-success d-block d-md-inline-block"
+												>
+													Edit customer
+												</Link>
+											</div>
+										</div>
+									</li>
+								))}
+							</ul>
+						</Fragment>
+					);
+				}}
+			</Query>
+		);
+	}
+}
